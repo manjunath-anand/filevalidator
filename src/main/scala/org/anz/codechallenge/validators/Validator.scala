01e864d8b92bbf26de4605f315793e45fdc5cf21
manand15 <anand.manjunath@optum.com>
2022-04-03 13:16:08 +0530

    Add main validation logic

--- src/main/scala/org/anz/codechallenge/validators/Validator.scala
+++ src/main/scala/org/anz/codechallenge/validators/Validator.scala
@@ -1,7 +1,14 @@
 package org.anz.codechallenge.validators
 
+/**
+  * All validators will extend this trait
+  */
 trait Validator {
 
+  /**
+    * Perform data validation
+    * @return - status of validation
+    */
   def validate(): String
 
 }
